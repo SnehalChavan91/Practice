fa932bd1e95155138168508d2cf6c3d7f07821e1
Snehal Chavan <chavansnehalh91@gmail.com>
2024-07-25 18:38:41 +0530

    Addition program with some changes

--- addition.js
+++ addition.js
@@ -3,3 +3,8 @@ const num2 = parseInt(prompt('Enter second number '));
 const sum = num1 + num2;
 console.log(`The sum of ${num1} and ${num2} is ${sum}`);
 alert(sum)
+console.log("Hello everyone");
+var a = 10;
+var b = 20;
+var c = a+b;
+console.log(c)
