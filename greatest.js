5386c68eb103ce518701f96407fcd516ac1cfb08
Snehal Chavan <chavansnehalh91@gmail.com>
2024-07-26 12:35:47 +0530

    Greatest number using Math.max()

--- greatest.js
+++ greatest.js
@@ -1,9 +1,9 @@
-const prompt = require("prompt-sync")();
+// const prompt = require("prompt-sync")();
 console.log(`Hello`);
 num1 = parseFloat(prompt("Enter first number: "));
 num2 = parseFloat(prompt("Enter the second number: "));
 num3 = parseFloat(prompt("Enter the third number: "));
-
+let greatest;
 if (num1 >= num2 && num1 >= num3) {
     greatest = num1;
 }
@@ -15,3 +15,6 @@ else {
 }
 console.log(`The greatest number among ${num1} , ${num2} and ${num3} is ${greatest} `);
 console.log("The greatest number is "+greatest);
+ 
+greatest = Math.max(num1 , num2 , num3)
+console.log(`The greatest number is ${greatest} .`)
