ec81064ea1cd2bfe2bf098e01513e39ba5beac9c
Snehal Chavan <chavansnehalh91@gmail.com>
2024-07-29 11:27:57 +0530

    Sum using recursion

--- sum.js
+++ sum.js
@@ -1,7 +1,27 @@
-//const prompt = require("prompt-sync")();
-const number = parseFloat(prompt("Enter the number: "));
-let sum = 0;
-for (let i = 1; i <= number; i++) {
-    sum +=i;
+//zconst prompt = require("prompt-sync")();
+// const number = parseFloat(prompt("Enter the number: "));
+// let sum = 0;
+// for (let i = 1; i <= number; i++) {
+//     sum +=i;
+// }
+// console.log(`The sum of numbers is ${sum} .`);
+// let addition = 0, j = 1;
+// while(j <= number) {
+//     addition += j;
+//     j++;
+// }
+// console.log(`The addition of numbers is ${addition} .`)
+
+function sum(num) {
+    if(num > 0) {
+        return num + sum(num - 1);
+    }
+    else {
+        return num;
+    }
+    
 }
-console.log(`The sum of numbers is ${sum}`);
+const num = parseInt(prompt("Enter the number: "));
+const result = sum(num);
+
+console.log(`The sum of numbers is : ${result}`);
