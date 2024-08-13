3a589dba46f25a152ef95b1ae01417be9daf6b88
Snehal Chavan <chavansnehalh91@gmail.com>
2024-08-13 15:51:11 +0530

    Find out longest word among words from list

---
+++ Python/string/length_of_longest.py
@@ -0,0 +1,11 @@
+import math
+def longest_word(words):
+    words_list=[]
+    for i in words:
+        words_list.append((len(i),i))
+        words_list.sort()
+    return words_list[-1][0],words_list[-1][1]
+words=input("Enter the words")
+words=words.split()
+print(longest_word(words))
+
