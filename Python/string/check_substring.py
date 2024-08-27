2b20ed7ff18a42712df08b6b3efe0a8ed2ff4113
Snehal Chavan <chavansnehalh91@gmail.com>
2024-08-27 17:28:57 +0530

    Python program to count occurrences of a substring in a string

---
+++ Python/string/check_substring.py
@@ -0,0 +1,5 @@
+# Python program to count occurrences of a substring in a string.
+def substring_occurance(str1):
+    a=print(str1.count("is"))
+    return a
+print(substring_occurance("Python is a programming language. It is interpreted in nature."))
